56848a1aa3f45a367af5444c95caeba410ca5a5d
reza <reezzaa12@gmail.com>
2024-01-06 21:03:35 +0100

    first commit

---
+++ calcclass/calculator.cs
@@ -0,0 +1,24 @@
+namespace calcclass;
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+    
+    public int Substract(int a, int b)
+    {
+        return 0;
+    }
+
+    public int Multiply(int a, int b)
+    {
+        return 0;
+    }
+
+    public int Division(int a, int b)
+    {
+        return 0;
+    }
+
+}
